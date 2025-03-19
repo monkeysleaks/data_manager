e205c0eef340abb904588648b578a4a0088b548a
soteiro <diego_sarq@hotmail.com>
2025-03-19 16:45:13 -0300

    chache y settings

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from src.upload.uploader_voe import Voe
+from src.upload import uploader_filemoon as filemoon
 from dotenv import load_dotenv
 from icecream import ic
 import os
@@ -9,6 +10,7 @@ def main():
     try:
 
         token_voe = os.environ.get('API_KEY_VOE')
+        token_filemoon = os.environ.get('API_KEY_FILEMOON')
 
         while True:
             print('''1.- Subir voe
@@ -23,6 +25,7 @@ def main():
             opcion = int(input("ingrese opcion (1-7): "))
             
             if opcion == 1:
+
                 while True:
                     print('''
         1.- Carpeta Existente
@@ -39,13 +42,34 @@ def main():
                         Voe.main_upload_voe(artista)
                     elif subopcion1 == 3:
                         break
+           
             elif opcion == 2:
                 while True:
                     print('''
-1.- nuevos archivos
-2.- archivos 
- ''')
+    1.- Nuevos archivos
+    2.- resubir Archivos (raro?)
+    3.- Volver Atrás
+ ''')           
+                    subopcion2 = int(input("ingrese opción (1-3): "))
+                    if subopcion2 == 1:
+                        artista = input("ingrese artista: ")
+                        filemoon.create_folder(token_filemoon, artista)
+                        # filemoon.main_upload_filemoon(artista)
+
+                    elif subopcion2 == 2:
+                        artista = input("ingrese artista: ")
+                        pass
+
+                    elif subopcion2 == 3:
+                        break
+
+            elif opcion == 3:
+                #agregar nuevos datos a la db
+                pass
+
             elif opcion == 8:
+
+                ic("saludos Que Tengas buen día")
                 break
             
 
