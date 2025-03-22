eb0226812b71ce4bea3e1b80c9058725443314d1
soteiro <diego_sarq@hotmail.com>
2025-03-21 21:36:52 -0300

    cambio de env

--- src/utils/rebuild.py
+++ src/utils/rebuild.py
@@ -4,13 +4,13 @@ from dotenv import load_dotenv
 load_dotenv()
 
 client = Cloudflare(
-    api_email= os.getenv("CLOUDFLARE_EMAIL"),  # This is the default and can be omitted
-    api_key=os.getenv("CLOUDFLARE__API_KEY"),  # This is the default and can be omitted
+    api_email= os.environ("CLOUDFLARE_EMAIL"),  # This is the default and can be omitted
+    api_key=os.environ("CLOUDFLARE__API_KEY"),  # This is the default and can be omitted
 )
 def obtener_id_deploment():
     page = client.pages.projects.deployments.list(
-        project_name=os.getenv("CF_PROJECT_NAME"),
-        account_id=os.getenv("CF_ACCOUNT_ID"),
+        project_name=os.environ("CF_PROJECT_NAME"),
+        account_id=os.environ("CF_ACCOUNT_ID"),
     )
     page = page.result[0]
     # print(page.id)
@@ -19,8 +19,8 @@ def obtener_id_deploment():
 def retry_deployment(deploy_id):
     deployment = client.pages.projects.deployments.retry(
         deployment_id=deploy_id,
-        account_id=os.getenv("CF_ACCOUNT_ID"),
-        project_name=os.getenv("CF_PROJECT_NAME"),
+        account_id=os.environ("CF_ACCOUNT_ID"),
+        project_name=os.environ("CF_PROJECT_NAME"),
         body={},
     )
 
@@ -32,10 +32,12 @@ def main():
         api_email=os.environ.get("CLOUDFLARE_EMAIL"),  # This is the default and can be omitted
         api_key=os.environ.get("CLOUDFLARE_API_KEY"),  # This is the default and can be omitted
     )
-    deploy_id =obtener_id_deploment()
-    retry_deployment(deploy_id)
-    print("ejecucion completa") 
-
+    try:
+        deploy_id = obtener_id_deploment()
+        retry_deployment(deploy_id)
+        print("ejecucion completa") 
+    except Exception as e:
+        print(e)
 if __name__ == "__main__":
-     main()
+    main()
     
